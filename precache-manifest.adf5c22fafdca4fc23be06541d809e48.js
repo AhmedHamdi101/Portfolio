21091b604d214f7b2ac1505e25768156ca0f18f5
Ahmed Hamdi <ahmed.moh.hamdi@gmail.com>
2021-09-06 23:07:06 +0200

    Updates

---
+++ precache-manifest.adf5c22fafdca4fc23be06541d809e48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaea25695bf60d7537a89ab78bc1ab8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d00a4012810d0c8d8511",
+    "url": "/home/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "a3e93bc312eb5f201c83",
+    "url": "/home/static/js/2.4fc7f5be.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.4fc7f5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00a4012810d0c8d8511",
+    "url": "/home/static/js/main.feb164ac.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "d6f495a780176603756b5112ed737568",
+    "url": "/home/static/media/ahmedhamdi2.d6f495a7.jpg"
+  },
+  {
+    "revision": "a98c79980ee97acb3ac01aef064b9689",
+    "url": "/home/static/media/gradteam.a98c7998.jpg"
+  },
+  {
+    "revision": "73f53b4a00c586bbcaff55ea9e068529",
+    "url": "/home/static/media/monodepth.73f53b4a.jpg"
+  },
+  {
+    "revision": "4e0a9d86c8ab7e1ef4bb8d7c4ea0642e",
+    "url": "/home/static/media/resume.4e0a9d86.pdf"
+  },
+  {
+    "revision": "64bfb1724dfe484a73fc5dd4be8336f2",
+    "url": "/home/static/media/yolo.64bfb172.jpg"
+  }
+]);
